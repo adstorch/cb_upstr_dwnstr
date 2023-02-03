3de584952c93e7cc5f7dddff2f27b8483b9ad48f
Adam J Storch <Adam.J.STORCH@odfw.oregon.gov>
2023-02-03 14:46:00 -0800

    update

--- Scripts/cb_upstr_dwnstr_data_pkgs.R
+++ Scripts/cb_upstr_dwnstr_data_pkgs.R
@@ -29,4 +29,11 @@ cb_upstr_dwnstr.dat <- read.xlsx("Data\\Input Data\\cb_upstr_dwnstr_inp_data.xls
                          colNames = TRUE)
 
 ## data manipulation
-cb_upstr_dwnstr.dat <- subset(cb_upstr_dwnstr.dat, brd_yr>=2000|brd_yr>=2014)
+cb_upstr_dwnstr.dat <- subset(cb_upstr_dwnstr.dat, brd_yr>=2000 & brd_yr<=2015)
+cb_upstr_dwnstr.dat$rs <- cb_upstr_dwnstr.dat$nat_recruits/cb_upstr_dwnstr.dat$tot_spnrs
+cb_upstr_dwnstr.dat$basin_index <- ifelse(cb_upstr_dwnstr.dat$basin=="sr",1,2)
+
+### covariate placeholders (for framework development only)
+cb_upstr_dwnstr.dat$cov1 <- rnorm(nrow(cb_upstr_dwnstr.dat),20,3)
+cb_upstr_dwnstr.dat$cov2 <- rnorm(nrow(cb_upstr_dwnstr.dat),20,4)
+cb_upstr_dwnstr.dat$cov3 <- rnorm(nrow(cb_upstr_dwnstr.dat),20,10)
