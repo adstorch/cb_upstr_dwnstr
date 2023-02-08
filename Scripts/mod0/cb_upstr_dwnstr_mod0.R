06ed3b2cd778e2e1f48c9da84d0a6dff94957228
Adam J Storch <Adam.J.STORCH@odfw.oregon.gov>
2023-02-08 11:53:18 -0800

    update

--- Scripts/mod0/cb_upstr_dwnstr_mod0.R
+++ Scripts/mod0/cb_upstr_dwnstr_mod0.R
@@ -27,7 +27,8 @@ cat('
       # mu_pop[pop_vec[i]] ~ dnorm(mu_basin[basin_vec[i]], taub_a)
       
       log_RS[i] ~ dnorm(mu_log_RS[i],tau)
-      mu_log_RS[i] <- lnalpha - beta * S[i] + b1 * cov1[i] + b2 * cov2[i] + b3 * cov3[i]
+      # centered (continuous) covariates
+      mu_log_RS[i] <- lnalpha - beta * (S[i] - mean(S[]) + b1 * (cov1[i]-mean(cov1[]) + b2 * (cov2[i]-mean(cov2[]) + b3 * (cov3[i]-mean(cov3[])
       
       # + b1 * basin[]
       
